2dffdd8e088ff467ddff2ee524ff2884028037a1
wallcrawler <ronan_mccabe@hotmail.com>
2015-06-09 12:10:43 +0100

    heroku

--- serverHandler.js
+++ serverHandler.js
@@ -1,11 +1,22 @@
 var handlers = require('./handlers.js');
 var fs = require('fs');
+var pg = require('pg'); //postgres
 var index = fs.readFileSync(__dirname + '/index.html');
 
 function serverHandler (req, res){
+
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    var query = client.query('SELECT * FROM your_table');
+
+    query.on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+  });
+
   if (req.url.length === 1) {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.end(index.toString());
+    console.log(process.env.DATABASE_URL);
   }
 
   else {
