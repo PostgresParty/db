6a19d421b2441ff13d07ef03e7e70173dfc65df6
wallcrawler <ronan_mccabe@hotmail.com>
2015-06-09 16:59:52 +0100

    postgres going?

--- script.js
+++ script.js
@@ -3,10 +3,30 @@
     $( "#datepicker" ).datepicker();
   });
 
-  $('#submitName').click(function() {
-    var arr = [$('#nameInput').val(), $('#ageInput').val(), $('#jobInput').val(), $('#colorInput').val()];
-    $.post( '/addData', JSON.stringify(arr), function(data) {
-      console.log(data);
-    });
+  $('#submitButton').click(function() {
+    if($('#nameInput').val().length < 1 || $('#ageInput').val().length < 1 ||
+        $('#jobInput').val().length < 1 || $('#colorInput').val().length < 1) {
+      alert('Please fill in the required fields.');
+    } else {
+      var arr = [
+        $('#nameInput').val(), $('#ageInput').val(),
+        $('#jobInput').val(), $('#colorInput').val()
+      ];
+      $.post( '/addData', JSON.stringify(arr), function(data) {
+        console.log(data);
+      });
+
+      $('#nameInput').val('');
+      $('#ageInput').val('');
+      $('#jobInput').val('');
+      $('#colorInput').val('');
+    }
   });
+
+  $('.dataInput').keypress(function(e){
+    if(e.keyCode == 13) {
+      $('#submitButton').click();
+    }
+  });
+
 }());
