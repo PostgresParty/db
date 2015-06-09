00f740e6fee6a99a483db4b655d6200d55c4847c
wallcrawler <ronan_mccabe@hotmail.com>
2015-06-09 12:20:47 +0100

    heroku

--- serverHandler.js
+++ serverHandler.js
@@ -5,13 +5,13 @@ var index = fs.readFileSync(__dirname + '/index.html');
 
 function serverHandler (req, res){
 
-  pg.connect(process.env.DATABASE_URL, function(err, client) {
-    var query = client.query('SELECT * FROM your_table');
+  // pg.connect(process.env.DATABASE_URL, function(err, client) {
+  //   var query = client.query('SELECT * FROM your_table');
 
-    query.on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-  });
+  //   query.on('row', function(row) {
+  //     console.log(JSON.stringify(row));
+  //   });
+  // });
 
   if (req.url.length === 1) {
     res.writeHead(200, {"Content-Type": "text/html"});
